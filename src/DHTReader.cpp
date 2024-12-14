de29d4b438b05480e3ee38929a6e36b155d10282
SergiSG <sergi.salgueiro@wallbox.com>
2024-12-14 11:08:38 +0100

    first read

--- src/DHTReader.cpp
+++ src/DHTReader.cpp
@@ -11,7 +11,7 @@ void DHTReader::set_read_interval(uint interval)
 
 bool DHTReader::read_data(TempAndHumidity &data)
 {
-    if (millis() - last_read_time < read_interval_ms)
+    if (millis() - last_read_time < read_interval_ms || last_read_time == 0)
     {
         return false;
     }
